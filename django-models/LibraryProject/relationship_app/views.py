af2794a8f22c0f4058cf07495249972b5ab4d71f
Econi Noel Aita <noelaita03@gmail.com>
2025-08-09 16:34:37 +0300

    Added custom permissions to the book model

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.http import HttpResponse
-from django.contrib.auth.decorators import  permission_required, user_passes_test
+from django.contrib.auth.decorators import  permission_required
+from django.contrib.auth.decorators import user_passes_test
 
 from django.views.generic.detail import DetailView
 from .models import Library
