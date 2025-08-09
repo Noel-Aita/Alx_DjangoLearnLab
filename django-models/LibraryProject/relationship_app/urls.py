15a272a0d686deadd5d0ea10de4a96191515bb49
Econi Noel Aita <noelaita03@gmail.com>
2025-08-09 15:42:01 +0300

    added authentication to the app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView,  register
+from .views import list_books, LibraryDetailView
+from . import views
 
 
 urlpatterns = [
@@ -8,5 +9,5 @@ urlpatterns = [
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
