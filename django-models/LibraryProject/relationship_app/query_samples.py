327c8d882bd3acd6793481ea4b5867c132ada603
Econi Noel Aita <noelaita03@gmail.com>
2025-08-09 12:39:47 +0300

    Edited the Author function in the file query.samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,7 +13,7 @@ django.setup()
 # 1. Query all books by a specific author
 def books_by_author(author_name):
     try:
-        author = Author.objects.get(name = author_name)
+        author = Author.objects.get(name=author_name)
         books = Book.objects.filter(author=author)
         return books
     except Author.DoesNotExist:
