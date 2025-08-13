fbb042bb1b76d53bd0c412b3504343dfad0ad667
Econi Noel Aita <noelaita03@gmail.com>
2025-08-13 08:56:42 +0300

    Added a psot to the blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
