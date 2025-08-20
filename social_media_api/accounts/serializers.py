1a1cde7c9286fa3f1a554b47159dd970c8f37ecf
Econi Noel Aita <noelaita03@gmail.com>
2025-08-20 20:26:15 +0300

    added a serializers to the form

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,7 +2,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
