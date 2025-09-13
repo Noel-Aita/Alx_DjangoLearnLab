fcacf3f76531a1a978557f26e19fe6becf6967cb
Econi Noel Aita <noelaita03@gmail.com>
2025-09-13 15:57:54 +0300

    Included custom user

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,31 +1,29 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import CustomUser
 
 
-
-
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-# Add new fields to admin forms
-fieldsets = (
-(None, {'fields': ('username', 'password')}),
-(_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
-(_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-(_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
 
-add_fieldsets = (
-(None, {
-'classes': ('wide',),
-'fields': ('username', 'email', 'password1', 'password2'),
-}),
-)
-
-
-list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-search_fields = ('username', 'first_name', 'last_name', 'email')
-ordering = ('username',)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
